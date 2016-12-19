56b47b94ea2dd6708317b6d244a2888676eada96
Laura Frank <ljfrank@gmail.com>
2016-12-19 12:16:04 -0500

    Adding more examples and a README to the build args directory

--- 20.build-args/test-build-arg.sh
+++ 20.build-args/test-build-arg.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-# set -e
-
-if [[ $FOO == "bar" ]] ; then
-  echo "Unencrypted build args are working as expected; the value of FOO is bar"
-  exit 0
-else
-  echo "Unencrypted build args are not working. FOO should be bar but instead it's $FOO"
+if [[ $UNENCRYPTED == "default" ]] ; then
+  echo "Unencrypted build args are not working. UNENCRYPTED should not be default."
   exit 1
+else
+  echo "UNENCRYPTED has been set to $UNENCRYPTED"
 fi
 
-if [[ $USER == "myencrypteduser" ]] ; then
-  echo "Encrypted build args are working as expected; the value of USER is $USER"
-  exit 0
-else
-  echo "Encrypted build args are not working. USER should be myencrypteduser but instead it's $USER"
+if [[ $ENCRYPTED == "default" ]] ; then
+  echo "Encrypted build args are not working. ENCRYPTED should not be default."
   exit 1
+else
+  echo "ENCRYPTED has been set to $ENCRYPTED"
 fi
 
-if [[ $SECRET == "supersecret" ]] ; then
-  echo "Encrypted build args file is working as expected; the value of SECRET is $SECRET"
-  exit 0
-else
-  echo "Encrypted build args file is not working. SECRET should be supersecret but instead it's $SECRET"
+if [[ $FROMFILE == "default" ]] ; then
+  echo "Encrypted build args file is not working. FROMFILE should not be default."
   exit 1
+else
+  echo "FROMFILE has been set to $FROMFILE"
 fi
 
