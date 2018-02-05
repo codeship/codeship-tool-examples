64b0e2e3f85848c082e085e43369a8c025b83007
Mark Phelps <mark.aaron.phelps@gmail.com>
2018-02-05 10:40:00 -0500

    Add example for using default env vars as docker build arg

--- 20.build-args/README.md
+++ 20.build-args/README.md
@@ -1,4 +1,4 @@
-## Build Arguments
+# Build Arguments
 
 For any service, you can declare [build arguments](https://docs.docker.com/compose/compose-file/#/args), which are values available to the image only at build time. For example, if you must pass the image a set of credentials in order to access an asset or repository when the image is built, you would pass that value to the image as a build argument.
 
--- 20.build-args/test-build-arg.sh
+++ 20.build-args/test-build-arg.sh
@@ -7,6 +7,13 @@ else
   echo "UNENCRYPTED has been set to $UNENCRYPTED"
 fi
 
+if [[ $CI_TIMESTAMP == "default" ]] ; then
+  echo "Environment build args are not working. CI_TIMESTAMP should not be default."
+  exit 1
+else
+  echo "CI_TIMESTAMP has been set to $CI_TIMESTAMP"
+fi
+
 if [[ $ENCRYPTED == "default" ]] ; then
   echo "Encrypted build args are not working. ENCRYPTED should not be default."
   exit 1
@@ -20,4 +27,3 @@ if [[ $FROMFILE == "default" ]] ; then
 else
   echo "FROMFILE has been set to $FROMFILE"
 fi
-
