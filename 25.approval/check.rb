4e3c4d184ef601c914b54d94dde02c57acedcc74
Ethan <ethangj@gmail.com>
2018-05-07 13:06:39 -0400

    Adding persistent manual approval test

---
+++ 25.approval/check.rb
@@ -0,0 +1,21 @@
+require 'redis'
+require 'retriable'
+
+def exit_if_not expected, current
+  puts "Expected: #{expected}"
+  puts "Current: #{current}"
+  exit(1) if expected != current
+end
+
+Retriable.configure do |c|
+  c.tries = 10
+end
+
+puts 'Redis'
+
+Retriable.retriable on: [ Errno::ECONNREFUSED, Redis::CannotConnectError ] do
+  puts 'retry'
+  @redis = Redis.new(host: 'redis')
+  @redis.set 'foo', 'bar'
+end
+exit_if_not @redis.get('foo'), 'bar'
